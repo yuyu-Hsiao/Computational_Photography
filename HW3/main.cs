661f694b9cd8b5f32e070ecf5b37a64923a87de4
Horace-Hsiao@Horace-laptop <horace70112@gmail.com>
2025-01-31 00:12:54 +0800

    update

---
+++ HW3/main.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class script : MonoBehaviour
+{
+    //------------------------ 文字
+    public Text gameOverText;
+    public Text startText;
+    public Text timeText;
+    public Text endtimeText;
+
+    //------------------------ 河內塔的方塊
+    public GameObject large;
+    public GameObject medium;
+    public GameObject small;
+
+    //------------------------ marker
+    public GameObject ImageTarget1;
+    public GameObject ImageTarget2;
+    public GameObject ImageTarget3;
+
+    //------------------------ 虛擬按鈕
+    public VirtualButtonBehaviour vb;
+
+    private float gameTime;
+    private bool isGameRunning;
+    private int minutes;
+    private int seconds;
+    private string timeString;
+
+    void Start()
+    {
+        //--------------------------------- 設定虛擬按鈕
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; i++)
+        {
+            vbs[i].RegisterOnButtonPressed(OnButtonPressed);
+            vbs[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+
+        //--------------------------------- 設置文字初始狀態
+        gameOverText.gameObject.SetActive(false);
+        startText.gameObject.SetActive(true);
+        endtimeText.gameObject.SetActive(false);
+
+
+        gameTime = 0f;              /*--- 初始時間 ---*/
+        isGameRunning = false;  /*--- 遊戲是否在進行中 ---*/
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameRunning)
+        {
+            gameTime += Time.deltaTime;
+            UpdateTimeDisplay();
+        }
+    }
+
+    void UpdateTimeDisplay()
+    {
+        //------------------------------------------ 將時間轉換為分鐘和秒的格式
+        minutes = Mathf.FloorToInt(gameTime / 60f);
+        seconds = Mathf.FloorToInt(gameTime % 60f);
+
+        //------------------------------------------ 格式化時間文本
+        timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeText.text = "Time: " + timeString;
+    }
+
+    int x = 0;
+    //---------------------------------------------------------- 按下按鈕
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        print(vb.name + "按鈕按下");
+        x++;
+
+        //-------------------------------------------------------------- 每個 marker 的位置
+        Vector3 target1WorldPosition = ImageTarget1.transform.position;
+        Vector3 target2WorldPosition = ImageTarget2.transform.position;
+        Vector3 target3WorldPosition = ImageTarget3.transform.position;
+
+        //-------------------------------------------------------------- 遊戲中的操作
+        if (x == 1)
+        {
+            isGameRunning = true;
+            startText.gameObject.SetActive(false);
+            small.transform.position = new Vector3(target3WorldPosition.x, target3WorldPosition.y, target3WorldPosition.z);
+        }
+        if (x == 2)
+        {
+            medium.transform.position = new Vector3(target2WorldPosition.x, target2WorldPosition.y, target2WorldPosition.z);
+        }
+
+        if (x == 3)
+        {
+            small.transform.position = new Vector3(target2WorldPosition.x, target2WorldPosition.y + 0.2f, target2WorldPosition.z);
+        }
+        if (x == 4)
+        {
+            large.transform.position = new Vector3(target3WorldPosition.x, target3WorldPosition.y, target3WorldPosition.z);
+        }
+        if (x == 5)
+        {
+            small.transform.position = new Vector3(target1WorldPosition.x, target1WorldPosition.y, target1WorldPosition.z);
+        }
+        if (x == 6)
+        {
+            medium.transform.position = new Vector3(target3WorldPosition.x, target3WorldPosition.y + 0.2f, target3WorldPosition.z);
+        }
+        if (x == 7)
+        {
+            small.transform.position = new Vector3(target3WorldPosition.x, target3WorldPosition.y + 0.4f, target3WorldPosition.z);
+        }
+        if (x == 8)
+        {
+            endtimeText.text = "Total Time: " + timeString;
+            gameOverText.gameObject.SetActive(true);
+            endtimeText.gameObject.SetActive(true);
+            isGameRunning = false;
+        }
+    }
+
+    //---------------------------------------------------------- 釋放按鈕
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Release");
+        print(vb.name + "按鈕放開");
+    }
+
+}
---
+++ HW3/readme.txt
@@ -0,0 +1,32 @@
+姓名：蕭宥羽
+學號：612415013
+e-mail：horace70112@gmail.com
+作業環境：Win11 64bit 
+編譯器：visual studio 2022
+
+指定作業功能:
+1. AR 啟動!
+    -在螢幕上顯示出現實環境和由 marker產生出的河內塔
+    -利用 unity 和 vuforia 實現開發
+
+2. 利用 marker 來跟河內塔做互動
+    -用marker上面虛擬按鈕控制河內塔
+    -每次只能移動一個圓盤
+    -大盤不能疊在小盤上面
+
+3. 用 unity 的按鈕實現離開跟開始的動作
+
+
+Bonus:
+1. 在 marker 上添加虛擬按鈕
+2. 在遊戲尚未開始之前顯示start!!!，請玩家按壓按鈕開始遊戲
+3. 按下按鈕後開始計時遊戲時間
+4. 結束遊戲後會顯示 game over，並告知玩家遊玩所花費之時間
+5. 顯示個可愛的小女孩物在 marker 上，增加整體美觀度
+
+
+參考:
+1. 與612415045同學討論，並一起完成此項作業
+2. https://developer.vuforia.com/
+3. https://assetstore.unity.com/packages/3d/haruko-69164
+
